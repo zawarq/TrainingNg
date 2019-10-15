f42f1ba66bbf868e2b4f673e8015c01c09188cab
drzeeman <zawarq@gmail.com>
2019-10-16 10:08:01 +1100

    Initial modifications and removed unnecessary startup files

--- TrainingNg/ClientApp/src/app/home/home.component.ts
+++ TrainingNg/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  public training: Training;
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    http.post<Training>(baseUrl + 'api/Training/Add', this.training).subscribe(result => {
+      this.training.duration = result;
+    }, error => console.error(error));
+  }
+}
+
+interface Training {
+  name: string;
+  start: string;
+  end: string;
+  duration: number;
 }
---
+++ TrainingNg/Controllers/TrainingController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingNg.Controllers
+{
+    [Route("api/[controller]")]
+    public class TrainingController : Controller
+    {        
+        [HttpPost("[action]")]
+        public int Post(Training t)
+        {
+            return t.Duration;
+        }
+
+        public class Training
+        {
+            public string Name { get; set; }
+            public DateTime Start { get; set; }
+            public DateTime End { get; set; }
+
+            public int Duration
+            {
+                get
+                {
+                    return Convert.ToInt32((End - Start).TotalDays);
+                }
+            }
+        }
+    }
+}
