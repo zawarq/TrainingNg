80df85fd97644f181b0be5d9c1fce79815fb096a
drzeeman <zawarq@gmail.com>
2019-10-17 01:35:28 +1100

    Added toastr notification. Cleared form on successful Add. Removed duration control.

--- TrainingNg/ClientApp/src/app/app.module.ts
+++ TrainingNg/ClientApp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -9,6 +10,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
+import { ToastrModule } from 'ng6-toastr-notifications';
 
 @NgModule({
   declarations: [
@@ -17,11 +19,13 @@ import { Ng4LoadingSpinnerModule } from 'ng4-loading-spinner';
     HomeComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     Ng4LoadingSpinnerModule.forRoot(),
+    ToastrModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
     ])
--- TrainingNg/ClientApp/src/app/home/home.component.ts
+++ TrainingNg/ClientApp/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { FormBuilder } from '@angular/forms';
 import { FormGroup, FormControl } from '@angular/forms';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
+import { ToastrManager } from 'ng6-toastr-notifications';
 
 @Component({
   selector: 'app-home',
@@ -15,22 +16,25 @@ export class HomeComponent {
   private trainingFormGroup: FormGroup;
   private formBuilder: FormBuilder;
   private spinner: Ng4LoadingSpinnerService;
+  private toastr: ToastrManager;
 
   constructor(
     http: HttpClient,
     @Inject('BASE_URL') baseUrl: string,
     formBuilder: FormBuilder,
-    spinnerService: Ng4LoadingSpinnerService) {
+    spinnerService: Ng4LoadingSpinnerService,
+    toastrManager: ToastrManager) {
+
     this.http = http;
     this.baseUrl = baseUrl;
     this.formBuilder = formBuilder;
     this.spinner = spinnerService;
+    this.toastr = toastrManager;
 
     this.trainingFormGroup = new FormGroup({
       name: new FormControl(),
       start: new FormControl(),
-      end: new FormControl(),
-      duration: new FormControl()
+      end: new FormControl()
     });
   }
 
@@ -40,17 +44,26 @@ export class HomeComponent {
 
     this.training = new Training(this.trainingFormGroup.value);
     
-    return this.http.post(this.baseUrl + 'api/Training/Post', this.training).subscribe(result => {
+    return this.http.post(this.baseUrl + 'api/Training/Post', this.training).subscribe(() => {
 
-      const startDate = new Date(this.training.start);
-      const endDate = new Date(this.training.end);
-      const diffTime = Math.abs(startDate.getTime() - endDate.getTime());
-      const duration = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
-
-      this.trainingFormGroup.controls['duration'].setValue(duration + ' days');
+      const duration = this.getDuration(this.training.start, this.training.end);
+      this.toastr.successToastr('<p>Training record saved successfully.</p>The training will last <b>' + duration + '</b> days.', 'Success', { enableHTML: true });
       this.spinner.hide();
+      this.emptyFill();
 
-    }, error => console.error(error));
+    }, error => {
+      this.toastr.errorToastr('Error saving training Record', 'Error');
+      console.error(error);
+    });
+  }
+
+  getDuration(start: string, end: string) {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    const diffTime = Math.abs(endDate.getTime() - startDate.getTime());
+    const duration = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1;
+
+    return duration;
   }
 
   testFill() {
@@ -58,8 +71,15 @@ export class HomeComponent {
       //validation sample: 'email': ['', Validators.compose([Validators.required])]
       'name': ["Sample Training"],
       'start': ["2019-10-01"],
-      'end': ["2019-10-15"],
-      'duration': ''
+      'end': ["2019-10-15"]
+    });
+  }
+
+  emptyFill() {
+    this.trainingFormGroup = this.formBuilder.group({
+      'name': [""],
+      'start': [""],
+      'end': [""]
     });
   }
 }
@@ -68,7 +88,6 @@ export class Training {
   name: string;
   start: string;
   end: string;
-  duration: number;
 
   public constructor(form: any) {
     this.name = form.name;
